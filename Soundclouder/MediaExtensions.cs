1e60d04f0af95d56927459a9647afa4d6c142749
Frederik P <frepbh@gmail.com>
2022-04-08 06:44:44 +0200

    Fixed a name.

--- Soundclouder/MediaExtensions.cs
+++ Soundclouder/MediaExtensions.cs
@@ -9,6 +9,6 @@ public static class MediaExtensions
 {
     public static Task<string> GetStreamURLAsync(this Media media) => API.GetStreamURLAsync(media.ClientInfo, media);
 
-    public static async Task Download(this Media media, string path, CancellationToken cancellationToken = default) =>
+    public static async Task DownloadAsync(this Media media, string path, CancellationToken cancellationToken = default) =>
         await FFmpeg.DownloadToPath(path, await media.GetStreamURLAsync(), cancellationToken);
 }
