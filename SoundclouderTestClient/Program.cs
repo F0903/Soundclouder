6ce55af71c9cc6c32835a769ee5bbd3233652def
Frederik P <frepbh@gmail.com>
2022-04-08 06:49:51 +0200

    Now searches for another song in the test client.

--- SoundclouderTestClient/Program.cs
+++ SoundclouderTestClient/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Soundclouder;
 
 var client = new SearchClient(new ClientInfo { ClientId = "tvr5oyEDbwmNuQSmuNFkGLFrMn5wqT3H", UserId = "823336-646462-507678-448882" });
-var result = await client.SearchAsync("tramper torben jødebussen");
+var result = await client.SearchAsync("melvv riot");
 var first = result.First();
 await first.DownloadAsync("./out.mp3");
